80fc0fb03f409612150a218e69a7d6b5a45304da
KateVolodko <katevolodko04@gmail.com>
2024-12-05 14:31:53 +0500

    some changes

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AppRoute } from './const'
 import MainScreen from './main-screen';
 import ComparisonScreen from './comparison-screen';
 import Header from './components/Header/Header';
@@ -6,19 +7,19 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  
-  return (
-    <Router>
-      <div className="App">
-        <Header title="My React App" subtitle="Welcome to the best app ever!" />
-        <Routes>
-          <Route path="/" element={<MainScreen />} />
-          <Route path="/comparison" element={<ComparisonScreen />} />
-        </Routes>
-        <Footer title="My React App" subtitle="Welcome to the best app ever!" />
-      </div>
-    </Router>
-  );
+
+	return (
+		<Router>
+			<div className="App">
+				<Header title="My React App" subtitle="Welcome to the best app ever!" />
+				<Routes>
+					<Route path={AppRoute.Root} element={<MainScreen />} />
+					<Route path={AppRoute.Comparison} element={<ComparisonScreen />} />
+				</Routes>
+				<Footer title="My React App" subtitle="Welcome to the best app ever!" />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ test/src/const.ts
@@ -0,0 +1,6 @@
+export enum AppRoute {
+	Root = '/',
+	Login = '/login',
+	Comparison = '/comparison',
+	Monitoring = '/monitoring'
+}
